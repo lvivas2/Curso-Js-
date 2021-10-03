1f2cc74ed6c3a237f1ff40fd69c94394bf05e2aa
lvivas <vivasleonardo4@gmail.com>
2021-10-03 18:15:04 -0300

    clase 1 - act 1 JS

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+let nombre = prompt('Ingrese su nombre') 
+let apellido = prompt('Ingrese su apellido') 
+
+
+let nombreCompleto = nombre + " " + apellido + " " + "Estudiante de Coder"
+
+console.log(nombreCompleto)
+
+const añoActual = 2021
+
+let año = prompt('Ingrese su año de nacimiento')
+
+año = Number( año )
+
+let edad = añoActual - año 
+
+console.log("Edad:" + edad)
+
+
