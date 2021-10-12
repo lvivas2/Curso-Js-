803e6bda34398afe76e04a68f4779176f30616df
lvivas <vivasleonardo4@gmail.com>
2021-10-12 20:58:04 -0300

    clase 3

---
+++ Clase 3/js/app.js
@@ -0,0 +1,25 @@
+
+// const limite = parseInt(prompt('Ingrese el limite'))
+
+// let Numero = Number(prompt('Ingrese un numero inicial'))
+
+
+// for (let i = 0; i < limite; i++) {
+//     console.log(Numero)
+
+//     Numero <= 10 
+
+   
+// }
+
+
+let numero = 0
+
+while(numero <= 10){
+    console.log("Valor actual:" + numero)
+    numero =  numero + 1
+}
+
+
+
+
